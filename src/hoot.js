86658aa1cfcee5b6aedbdc759c6582ad60876c40
Sargasm <kolbykruger@gmail.com>
2018-11-27 21:56:08 -0500

    Ironed out more of the base functionality for testing purposes

--- src/hoot.js
+++ src/hoot.js
@@ -11,12 +11,36 @@ let Hoot = (function() {
         item.addEventListener('mouseout', tooltipsDestroy);
     });
 
+    // Tooltip Creation Function
     function tooltipsCreate() {
+        // Set variable content based on data-attributes
+        let ttContent = this.dataset.tooltip;
+        let ttPos = this.dataset.tooltipPosition;
 
+        // Create a div element and assign attributes
+        let tooltip = document.createElement('div');
+        tooltip.setAttributes({
+            'id':'tg',
+            'class': 'tooltip',
+            'data-position': ttPos
+        });
+
+        // Create text node for tooltip content
+        let ttText = document.createTextNode(ttContent);
+        tooltip.appendChild(ttText);
+
+        // Append tooltip to document
+        document.body.appendChild(tooltip);
     }
 
+    // Tooltip Removal Function
     function tooltipsDestroy() {
+        let elemDestroy = document.querySelectorAll('.tt');
 
+        // Remove tooltips from document
+        elemDestroy.forEach(function(item) {
+            item.parentNode.removeChild(item);
+        });
     }
 
 })
